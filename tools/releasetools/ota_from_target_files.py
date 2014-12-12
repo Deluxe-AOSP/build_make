7b8b93470d5e67173230e0e802b618e2b959fbcf
Tom Marshall <tdm@cyngn.com>
2014-12-12 11:51:33 -0800

    build: ota: Support for install tools in /tmp/install
     * Anything in OUT/install gets packaged up into the zip and extracted
       to /tmp/install immediately after FullOTA_InstallBegin.
    
     * Use /tmp/install in edify scripts and remove code related to using
       and manipulating /system for install tools.
    
     * Modified to support signing steps being split from build steps.
    
       Package install files into target-files INSTALL path
       Read from target-files for OTA package creation
    
       From Change-Id: I64f919c2a757b5474f6cc5f82bd6c33c2a8b558a
    
     * This also fully reverts commit 6a324ba and partially reverts
       commit f388104 as the functions are still needed here.
    
       From Change-Ids: I4911244ec9945d197d2b56d0d11eab6d2f7b6d3e
                        I4943e2e89ee5c810a63746c570dc5e31e95b8c53
    
    Squashed with the following:
    
      Author: LuK1337 <priv.luk@gmail.com>
      Date:   Wed Feb 19 02:14:59 2020 +0100
    
        releasetools: Use 0oXXX instead of 0XXX for octal
    
        * Fixes py3 syntax error.
    
        Change-Id: Ia9ca6e392f43694ddf4c952b07bf159e8dead36e
    
    Change-Id: I315a3238e36c8d15e26f935e272f7e27dd59c320

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -272,7 +272,7 @@ METADATA_NAME = 'META-INF/com/android/metadata'
 POSTINSTALL_CONFIG = 'META/postinstall_config.txt'
 DYNAMIC_PARTITION_INFO = 'META/dynamic_partitions_info.txt'
 AB_PARTITIONS = 'META/ab_partitions.txt'
-UNZIP_PATTERN = ['IMAGES/*', 'META/*', 'OTA/*', 'RADIO/*', 'INSTALL/*']
+UNZIP_PATTERN = ['IMAGES/*', 'INSTALL/*', 'META/*', 'OTA/*', 'RADIO/*']
 # Files to be unzipped for target diffing purpose.
 TARGET_DIFFING_UNZIP_PATTERN = ['BOOT', 'RECOVERY', 'SYSTEM/*', 'VENDOR/*',
                                 'PRODUCT/*', 'SYSTEM_EXT/*', 'ODM/*']
@@ -825,10 +825,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   CopyInstallTools(output_zip)
   script.UnpackPackageDir("install", "/tmp/install")
-  script.SetPermissionsRecursive("/tmp/install", 0, 0, 0755, 0644, None, None)
-  script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0755, 0755, None, None)
-
-  script.Print("BackupTool Work Done");
+  script.SetPermissionsRecursive("/tmp/install", 0, 0, 0o755, 0o644, None, None)
+  script.SetPermissionsRecursive("/tmp/install/bin", 0, 0, 0o755, 0o755, None, None)
 
   # All other partitions as well as the data wipe use 10% of the progress, and
   # the update of the system partition takes the remaining progress.
