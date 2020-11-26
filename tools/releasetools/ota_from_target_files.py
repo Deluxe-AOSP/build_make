fb810a8d48e480858a6f12aee8faa380e3c19e97
Rajkale99 <kaleraj.rk@gmail.com>
2020-11-26 19:44:08 +0000

    Revert "build: Force system-as-root layout for backuptool"
    This reverts commit acf0d3fd46636bb477388c2ccd065c61c9a382eb.

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -835,7 +835,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.Mount("/system")
-    script.RunBackup("backup", "/system/system")
+    script.RunBackup("backup")
     script.Unmount("/system")
 
   script.Print("BackupTool Work Done");
@@ -912,7 +912,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
     script.Mount("/system")
-    script.RunBackup("restore", "/system/system")
+    script.RunBackup("restore")
     script.Unmount("/system")
 
   script.WriteRawImage("/boot", "boot.img")
